5ea3b22ecd71e50d17521daf83b0445880be8c17
Joey Pereira <joey@pereira.io>
2017-06-08 00:57:16 -0400

    Initial commit

---
+++ lib/monkey_script.ex
@@ -0,0 +1,18 @@
+defmodule MonkeyScript do
+  @moduledoc """
+  Documentation for MonkeyScript.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MonkeyScript.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/monkey_script_test.exs
@@ -0,0 +1,8 @@
+defmodule MonkeyScriptTest do
+  use ExUnit.Case
+  doctest MonkeyScript
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
